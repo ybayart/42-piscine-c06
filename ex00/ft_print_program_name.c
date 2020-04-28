fc84e8e612d7702ca3da9410a5979ed7e3a64063
ybayart <ybayart@student.42.fr>
2020-04-28 05:52:50 +0200

    init

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybayart <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/06 01:53:11 by ybayart           #+#    #+#             */
+/*   Updated: 2019/08/06 01:56:42 by ybayart          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i));
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc >= 1)
+		ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybayart <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/06 02:03:00 by ybayart           #+#    #+#             */
+/*   Updated: 2019/08/06 02:03:34 by ybayart          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(*(str + i));
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (++i < argc)
+		ft_putstr(argv[argc - i]);
+}
